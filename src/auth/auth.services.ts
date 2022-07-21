7dd7c8283ec81c49e8b7457e0d899143cb4ed40a
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-07-21 17:34:10 +0600

    basic setup done

--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-// Services/Providers are used for business login or funtionality
+// Services/Providers are used for business login or funtionality.
 
 import { Injectable } from '@nestjs/common';
 
